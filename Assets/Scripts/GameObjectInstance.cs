53d9d4f73e48c6dcc3e39fba8e11a5ac0f116e74
Xisca04 <114388909+Xisca04@users.noreply.github.com>
2023-10-24 17:15:14 +0200

    instate_randomPos

---
+++ Assets/Scripts/GameObjectInstance.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectInstance : MonoBehaviour
+{
+    public GameObject apple;
+   
+    private float _time = 1f;
+   
+    private int spawnPosX = 2;
+    private int spawnPosY = 2;
+
+    private void Start()
+    {
+        InvokeRepeating("AppleInstance", _time, _time);
+    }
+
+    private void AppleInstance()
+    {
+        Instantiate(apple, RandomSpawnPos(), apple.transform.rotation);
+    }
+
+    private Vector2 RandomSpawnPos() 
+    {
+        int randomX = Random.Range(-spawnPosX, spawnPosX);
+        int randomY = Random.Range(-spawnPosY, spawnPosY);
+        return new Vector2(randomX, randomY);
+    }
+}
